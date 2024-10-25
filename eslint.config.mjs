f02758c4c5ad7ac484829c5ed3b1ff70c5031921
voun <1528759333@qq.com>
2024-10-25 13:38:15 +0800

    first

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended'
+import reactPlugin from 'eslint-plugin-react'
+
+export default [
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  reactPlugin.configs.flat.recommended,
+  eslintPluginPrettier,
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    ignores: ['**/*.config.js', 'dist/**', 'node_modules/**'],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+]
